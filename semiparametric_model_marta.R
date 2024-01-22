31c976c58bd336fbe44f799a364e78bd73774e0e
marta-krych <114980477+marta-krych@users.noreply.github.com>
2024-01-22 17:05:48 +0100

    small changes in the model

--- semiparametric_model_marta.R
+++ semiparametric_model_marta.R
@@ -1,31 +1,35 @@
 
+seed <- 20012024
+
+### Pre-processing part
+
 parkinson <- read.csv("data/parkinson.csv")
 
 colnames(parkinson) <- parkinson[1,]
 parkinson <- parkinson[-1,]
 rownames(parkinson) <- 1:dim(parkinson)[1]
 
-sani <- which(startsWith(parkinson$`Participant code`, "HC"))
-malati <- which(startsWith(parkinson$`Participant code`, "PD"))
-rischio <- which(startsWith(parkinson$`Participant code`, "RBD"))
+control <- which(startsWith(parkinson$`Participant code`, "HC"))
+pd <- which(startsWith(parkinson$`Participant code`, "PD"))
+rbd <- which(startsWith(parkinson$`Participant code`, "RBD"))
 
 parkinson <- parkinson[,-1]
 
 parkinson <- type.convert(parkinson, as.is=T)
 
-parkinson$Category <- as.factor(c( rep("PD", 30) , rep("RBD", 50), rep("Control",50) ))
+parkinson$Category <- as.factor(c(rep("PD",30), rep("RBD",50), rep("Control",50)))
 
 
-# Extracting 'UPDRS III Total' column and removing 'Action tremor'
-updrsIII <- as.numeric(parkinson[rischio, 13])
-updrsIII.new <- updrsIII - (as.numeric(parkinson[rischio, 21]) + as.numeric(parkinson[rischio, 22]))
+# Extracting 'UPDRS III Total' column and subtracting 'Action tremor' from it
+updrsIII <- as.numeric(parkinson[rbd, 13])
+updrsIII.new <- updrsIII - (as.numeric(parkinson[rbd, 21]) + as.numeric(parkinson[rbd, 22]))
 
 
-# Dataset with only speech variables:
+# Dataset with only speech variables
 speech_dataset <- parkinson[,c(41:65)]
 colnames(speech_dataset) <- c('EST_r','RST_r','AST_r','DPI_r','DVI_r','GVI_r','DUS_r','DUF_r','RLR_r',
-                             'PIR_r','RSR_r','LRE_r','EST_m','RST_m','AST_m','DPI_m','DVI_m','GVI_m','DUS_m',
-                             'DUF_m','RLR_m','PIR_m','RSR_m','LRE_m','Category')
+                              'PIR_r','RSR_r','LRE_r','EST_m','RST_m','AST_m','DPI_m','DVI_m','GVI_m','DUS_m',
+                              'DUF_m','RLR_m','PIR_m','RSR_m','LRE_m','Category')
 
 
 ### Boxplots
@@ -44,6 +48,7 @@ for (feature in features) {
 }
 
 
+
 ### Checking for speech variables with Gaussian distribution
 
 filtered_data <- subset(data, Category %in% c("PD", "Control"))
@@ -51,6 +56,8 @@ features <- names(filtered_data)[names(filtered_data) != "Category"]
 
 normality_test_results <- list()
 normal_variables <- c()
+
+set.seed(seed)
 
 for (feature in features) {
   pd_data <- filtered_data[filtered_data$Category == "PD", feature]
@@ -72,13 +79,41 @@ print(normal_variables)
 # "AST_r" "GVI_r" "RLR_r" "RST_m" "AST_m" "GVI_m" "DUF_m" "RLR_m" "RSR_m"
 
 
+
+### Checking for variance homogeneity of the Gaussian variables
+
+library(car)
+
+homogeneity_test_results <- list()
+homogeneous_normal_variables <- c()
+
+set.seed(seed)
+
+for (feature in normal_variables) {
+  
+  homogeneity_test_results[[feature]] <- list(var.test(filtered_data[filtered_data$Category == 'PD', feature],
+                                                       filtered_data[filtered_data$Category == 'Control', feature])$p.value)
+  
+  if (homogeneity_test_results[[feature]] > 0.05) {
+    homogeneous_normal_variables <- c(homogeneous_normal_variables, feature)
+  }
+}
+
+print(homogeneous_normal_variables)
+
+# The following variables are normally distributed with homogeneous variance:
+# "AST_r" "GVI_r" "RLR_r" "RST_m" "GVI_m" "DUF_m" "RLR_m" "RSR_m"
+
+
+
 ### Testing the normally distributed variables for the difference in mean between HC and PD
 
 significancy_test_results <- list()
 significant_normal_variables <- c()
 
+set.seed(seed)
 
-for (feature in normal_variables) {
+for (feature in homogeneous_normal_variables) {
   pd_data <- filtered_data[filtered_data$Category == "PD", feature]
   hc_data <- filtered_data[filtered_data$Category == "Control", feature]
   
@@ -93,17 +128,19 @@ for (feature in normal_variables) {
 
 print(significant_normal_variables)
 
-# Among the normally distributed speech variables, only "RST_m" seems to be significant
+# Among the normally distributed speech variables, only "RST_m" is significant
 
 
 
-### Testing the non-normally distributed variables for the difference in mean between HC and PD
+### Testing the non-normally distributed variables for the difference in median between HC and PD
 ### (using a permutation test)
 
-not_normal_variables <- setdiff(features, normal_variables)
+not_normal_variables <- setdiff(features, homogeneous_normal_variables)
 
 n_permutations <- 1000
 significant_p_values <- c()
+
+set.seed(seed)
 
 for (feature in not_normal_variables) {
   feature_data <- filtered_data[, feature]
@@ -125,8 +162,9 @@ for (feature in not_normal_variables) {
 
 print(significant_p_values)
 
-# Among the not-normally distributed speech variables, "RST_r" "DPI_r" "DUS_r" "DPI_m" 
+# Among the non-normally distributed speech variables, "RST_r" "DPI_r" "DUS_r" "DPI_m" 
 # have significant p-values
+
 
 
 ### Boxplots of significant features (HC vs PD)
@@ -146,51 +184,18 @@ for (feature in features) {
 }
 
 
-### Semiparametric model
 
-library(car)
+### Modeling step
+
 library(mgcv)
 library(splines)
 
-## Building a model with these variables: RST_r DPI_r DUS_r DPI_m RST_m
-
-df_model <- filtered_data[,c(25,2,4,7,14,16)]
-df_model$Category <- ifelse(df_model$Category == 'PD',1,0)
-
-with(df_model, scatterplotMatrix(data.frame(Category, RST_r, DPI_r, DUS_r, DPI_m, RST_m)))
-
-# linear relationship: RST_r, RST_m
-# non-linear relationship: DPI_r, DPI_m, DUS_r
-
-model_semipar <- gam(Category ~ RST_r + RST_m + s(DPI_r) + s(DPI_m) + s(DUS_r), 
-                     family=binomial, data=df_model)
-summary(model_semipar)
-
-test <- speech_dataset[rischio, c(2,14,4,16,7)]
-
-predictions <- predict(model_semipar, test, se=TRUE, type="response")
-# predictions <- predict(model_semipar, test, se=TRUE)
-# pfit <- exp(predictions$fit)/(1 + exp(predictions$fit))
-
-
-model_RBD <- ifelse(predictions$fit > .5, 1, 0)
-updrs_RBD <- ifelse(updrsIII.new > 3, 1, 0)
-
-table(true_label=updrs_RBD, prediction=model_RBD)
-
-cols <- ifelse(updrs_RBD == 1, 'red', 'black')
-plot(predictions$fit, col=cols, pch=16)
-abline(0.5, 0, col='red', lty=2, lwd=2)
-# The threshold itself is okay. It is the model that doesn't perform too well
-
-
-
-## Adding 'Gender' to the previous model (Gender RST_r DPI_r DUS_r DPI_m RST_m)
+## Building a model with: Gender RST_r DPI_r DUS_r RST_m DPI_m 
 
 df_model_g <- filtered_data[,c(25,2,4,7,14,16)]
 df_model_g$Category <- ifelse(df_model_g$Category == 'PD',1,0)
 
-df_model_g <- data.frame(Gender=parkinson[c(1:30, 81:130),2], df_model_g)
+df_model_g <- data.frame(Gender=parkinson[c(pd, control),2], df_model_g)
 df_model_g$Gender <- ifelse(df_model_g$Gender == 'F',1,0)
 
 with(df_model_g, scatterplotMatrix(data.frame(Category, Gender, RST_r, DPI_r, DUS_r, DPI_m, RST_m)))
@@ -198,18 +203,24 @@ with(df_model_g, scatterplotMatrix(data.frame(Category, Gender, RST_r, DPI_r, DU
 # linear relationship: Gender, RST_r, RST_m
 # non-linear relationship: DPI_r, DPI_m, DUS_r
 
-model_semipar_g <- gam(Category ~ Gender + RST_r + RST_m + s(DPI_r) + s(DPI_m) + s(DUS_r), 
+cor(as.matrix(df_model_g[,c(3,6)]))  
+# cor(RST_r, RST_m) = 0.6322871 (highly correlated) 
+# ==> keeping just RST_r to avoid collinearity in the linear part of the model
+
+model_semipar_g <- gam(Category ~ Gender + RST_r + s(DPI_r) + s(DPI_m) + s(DUS_r), 
                        family=binomial, data=df_model_g)
 summary(model_semipar_g)
 
-test <- speech_dataset[rischio, c(2,14,4,16,7)]
+concurvity(model_semipar_g)  # some issues with DPI_r (high concurvity), should we deal with it somehow?
+# (concurvity is the analogous of collinearity but for smoothed terms)
+
+test <- speech_dataset[rbd, c(2,4,16,7)]
 test <- data.frame(Gender=ifelse(parkinson[31:80, 2] == 'F',1,0),
                    test)
 
 predictions <- predict(model_semipar_g, test, se=TRUE, type="response")
 # predictions <- predict(model_semipar_g, test, se=TRUE)
 # pfit <- exp(predictions$fit)/(1 + exp(predictions$fit))
-
 
 model_RBD <- ifelse(predictions$fit > .5, 1, 0)
 updrs_RBD <- ifelse(updrsIII.new > 3, 1, 0)
@@ -227,23 +238,23 @@ abline(0.5, 0, col='red', lty=2, lwd=2)
 library(conformalInference)
 
 train_gam <- function(x, y, out=NULL){
-  colnames(x) <- c('Gender','RST_r','RST_m','DPI_r','DPI_m','DUS_r')
+  colnames(x) <- c('Gender','RST_r','DPI_r','DPI_m','DUS_r')
   train_data <- data.frame(y, x)
-  model_gam <- gam(as.factor(y) ~ Gender + RST_r + RST_m + s(DPI_r) + s(DPI_m) + s(DUS_r),
+  model_gam <- gam(as.factor(y) ~ Gender + RST_r + s(DPI_r) + s(DPI_m) + s(DUS_r),
                    family=binomial, data=train_data)
 }
 
 predict_gam <- function(obj, new_x){
   new_x <- data.frame(new_x)
-  colnames(new_x) <- c('Gender','RST_r','RST_m','DPI_r','DPI_m','DUS_r')
+  colnames(new_x) <- c('Gender','RST_r','DPI_r','DPI_m','DUS_r')
   predict.gam(obj, new_x, type="response")
 }
 
-model_gam <- gam(Category ~ Gender + RST_r + RST_m + s(DPI_r) + s(DPI_m) + s(DUS_r), 
+model_gam <- gam(Category ~ Gender + RST_r + s(DPI_r) + s(DPI_m) + s(DUS_r), 
                  family=binomial, data=df_model_g)
 
 target <- df_model_g$Category
-covariates <- df_model_g[,c(1,3,6,4,7,5)]
+covariates <- df_model_g[,c(1,3,4,7,5)]
 
 c_preds <- conformal.pred(covariates, target, as.matrix(test), alpha=0.05, verbose=TRUE, 
                           train.fun=train_gam, predict.fun=predict_gam, num.grid.pts=200)
@@ -258,7 +269,6 @@ updrs_RBD <- ifelse(updrsIII.new > 3, 1, 0)
 
 table(true_label=updrs_RBD, prediction=model_RBD)
 
-cols <- ifelse(updrs_RBD == 1, 'red', 'black')
-plot(c_preds$pred , col=cols, pch=16)
-abline(0.5, 0, col='red', lty=2, lwd=2)
+
+
 
